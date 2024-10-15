e7cf2d70ef9ced6cfb53e3d7c88be3df0c5f2b89
Klapeks <sashaKaraguts@gmail.com>
2024-10-16 02:41:23 +0300

    Update db.creator.ts

--- src/database/db.creator.ts
+++ src/database/db.creator.ts
@@ -1,8 +1,14 @@
 import logger from "../utils/logs";
+import mPath from 'path';
 
 export function createDatabaseIfNotExists(typeormDataSource: any) {
     if (typeormDataSource._typeormExtension) return;
-    typeormDataSource._typeormExtension = require('typeorm-extension');
+    try {
+        typeormDataSource._typeormExtension = require('typeorm-extension');
+    } catch (err) {
+        const typeormExtPath = mPath.join(process.cwd(), 'node_modules/typeorm-extension');
+        typeormDataSource._typeormExtension = require(typeormExtPath);
+    }
     const _initialize = typeormDataSource.initialize.bind(typeormDataSource);
     typeormDataSource.initialize = async () => {
         await typeormDataSource._typeormExtension.createDatabase({

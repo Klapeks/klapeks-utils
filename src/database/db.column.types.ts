28cd63bb70bce04a52a686dfec9a74968fb0e7b9
Klapeks <sashaKaraguts@gmail.com>
2025-01-21 02:36:43 +0200

    0.0.13 - db type

--- src/database/db.column.types.ts
+++ src/database/db.column.types.ts
@@ -1,23 +1,27 @@
 import { assertNever } from "../utils/utils";
+import { DatabaseOptions } from "./db.types";
 import { getDatabaseType } from "./dbpath.parser"
 
 
-
 export function getDatabaseColumnTypes(): {
+    dbtype: DatabaseOptions['type'],
     enum: "enum" | "simple-enum",
-    datetime: "datetime" | "timestamp"
+    datetime: "datetime" | "timestamp",
 } {
     const type = getDatabaseType();
     switch (type) {
         case "sqlite": return {
+            dbtype: type,
             enum: "simple-enum",
             datetime: "datetime"
         }
         case "mysql": return {
+            dbtype: type,
             enum: "enum",
             datetime: "datetime"
         }
         case "postgres": return {
+            dbtype: type,
             enum: "enum",
             datetime: "timestamp"
         }
